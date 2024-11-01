f97e46f4a45e0d1c51b8ea12d7c7533b94d98fc4
Cecilia Forsberg Andersson <cecilia.forsbergand@learnet.se>
2024-11-01 14:50:34 +0100

    Added method AddRoom to Lokal

--- Lokal.cs
+++ Lokal.cs
@@ -31,6 +31,98 @@ namespace Bokningssystem
         {
             IsBooked = false; // Reset booking status
         }
-        //Jag har ändrat här!!!
+        public static void AddRoom(List<Lokal> allRooms)
+        //Behövs det en return av listan för att spara nytt rum?
+        {
+            Console.WriteLine("Vad vill du lägga till?");
+            Console.WriteLine("1.Sal\n2.Grupprum");
+            String? selection = Console.ReadLine();
+            if (selection == "1")
+            {
+                Console.WriteLine("Ange önskat nummer på salen");
+                Byte roomNumber;
+                bool roomNumberCheck = byte.TryParse(Console.ReadLine(), out roomNumber);
+                if (roomNumberCheck == true && roomNumber > 0)
+                {
+                    if (allRooms.FindIndex(x => x.RoomNumber == roomNumber) == -1)
+                    {
+                        Console.WriteLine("Finns det en projektor i salen? Svara ja/nej");
+                        String? projectorOrNot = Console.ReadLine();
+                        if (String.IsNullOrEmpty(projectorOrNot))
+                        {
+                            while (String.IsNullOrEmpty(projectorOrNot))
+                            {
+                                Console.WriteLine("Svara ja/nej");
+                                Console.WriteLine("Finns det en projektor i salen?");
+                                projectorOrNot = Console.ReadLine();
+                            }
+                        }
+                        else if (projectorOrNot.ToLower() == "Ja".ToLower())
+                        {
+                            Sal newSal = new Sal("Sal", roomNumber, 40, true);
+                            allRooms.Add(newSal);
+                        }
+                        else if (projectorOrNot.ToLower() == "Nej".ToLower())
+                        {
+                            Sal newSal = new Sal("Sal", roomNumber, 40, false);
+                            allRooms.Add(newSal);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Rumsnummret finns redan");
+                    }
+                }
+                else if (roomNumberCheck == false)
+                {
+                    Console.WriteLine("Ogiltlig inmatning");
+                }
+            }
+            else if (selection == "2")
+            {
+                Console.WriteLine("Ange önskat nummer på Grupprummet");
+                Byte roomNumber;
+                bool roomNumberCheck = byte.TryParse(Console.ReadLine(), out roomNumber);
+                if (roomNumberCheck == true && roomNumber > 0)
+                {
+                    if (allRooms.FindIndex(x => x.RoomNumber == roomNumber) == -1)
+                    {
+                        Console.WriteLine("Finns det eluttag i rummet? Svara ja/nej");
+                        String? socketOrNot = Console.ReadLine();
+                        if (String.IsNullOrEmpty(socketOrNot))
+                        {
+                            while (String.IsNullOrEmpty(socketOrNot))
+                            {
+                                Console.WriteLine("Svara ja/nej");
+                                Console.WriteLine("Finns det eluttag i salen?");
+                                socketOrNot = Console.ReadLine();
+                            }
+                        }
+                        else if (socketOrNot.ToLower() == "Ja".ToLower())
+                        {
+                            Grupprum newRoom = new Grupprum("Grupprum", roomNumber, 10, true);
+                            allRooms.Add(newRoom);
+                        }
+                        else if (socketOrNot.ToLower() == "Nej".ToLower())
+                        {
+                            Grupprum newRoom = new Grupprum("Grupprum", roomNumber, 10, false);
+                            allRooms.Add(newRoom);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Rumsnummret finns redan: {roomNumber}");
+                    }
+                }
+                else if (roomNumberCheck == false)
+                {
+                    Console.WriteLine("Ogiltlig inmatning");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ogiltligt val");
+            }
+        }
     }
 }
