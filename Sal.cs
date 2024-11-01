f66da09891fc94df4816ea0d8b6741f8a35c5f07
Cecilia Forsberg Andersson <cecilia.forsbergand@learnet.se>
2024-11-01 09:48:09 +0100

    Added property Socket to Sal. Added constructor.

--- Sal.cs
+++ Sal.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Bokningssystem
 {
-    internal class Sal
+    internal class Sal: Lokal
     {
+        public bool Socket { get; set; }
+        public Sal(String roomType, byte roomNumber, int numberOfChairs, bool socket)
+        {
+            RoomType = roomType;
+            RoomNumber = roomNumber;
+            NumberOfChairs = numberOfChairs;
+            Socket = socket;
+        }
     }
 }
