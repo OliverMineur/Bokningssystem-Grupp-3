ee4f5e4b3a1276d81a3bdb4613f4afbff0958893
oliver mineur <olivermineur@gmail.com>
2024-11-04 12:19:44 +0100

    changed variable namnes to england

--- Sal.cs
+++ Sal.cs
@@ -11,7 +11,7 @@ internal class Sal : Lokal
         Socket = socket;
     }
 
-    public override bool Boka(DateTime startTid, TimeSpan duration, string bokadAv)
+    public override bool Boka(DateTime startTid, TimeSpan duration, string clientName)
     {
         if (IsBooked)
         {
@@ -30,10 +30,10 @@ internal class Sal : Lokal
         IsBooked = true;
         BookingStartTime = startTid;
         BookingDuration = duration;
-        ClientName = bokadAv;
+        ClientName = clientName;
         BookingID = new Random().Next(1000, 9999);
 
-        Console.WriteLine($"Salen är bokad av {bokadAv} från {startTid} i {duration.TotalHours} timmar.");
+        Console.WriteLine($"Salen är bokad av {clientName} från {startTid} i {duration.TotalHours} timmar.");
         return true;
     }
 
